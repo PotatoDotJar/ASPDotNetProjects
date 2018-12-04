c1fc54415fde88a6ca784cb9f8077f8fa03d61cf
RJ Nader <rj@potatosaucevfx.com>
2018-12-04 11:52:41 -0500

    Setup project

---
+++ SecurityTesting/SecurityTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityTesting.Startup))]
+namespace SecurityTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
